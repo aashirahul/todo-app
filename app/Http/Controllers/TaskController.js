6ebf89a359823b570451f51fab783bf99d7ce1c0
Ashima Shrivastava <ashima206@gmail.com>
2016-11-09 21:17:52 -0500

    added create method in Taskcontroller

--- app/Http/Controllers/TaskController.js
+++ app/Http/Controllers/TaskController.js
@@ -4,6 +4,30 @@ const Task = use("App/Model/Task")
 
 
 class TaskController {
+	* index (request,response){
+		let userId = request.param('id')
+		let listId = request.param("listId")
+
+	}
+
+	* create (request,response){
+		let userId = request.param('id')
+		let listId = request.param("listId")
+		let data = request.all()
+		data.todo_list_id = listId
+		console.log(data);
+		let task = yield Task.create(data)
+		response.status(201).json(task)
+		
+
+	}
+	* erase (request,response){
+
+	}
+	* update (request,response){
+
+	}
+
 
 }
 
--- app/Model/Task.js
+++ app/Model/Task.js
@@ -4,20 +4,7 @@ const Lucid = use('Lucid')
 
 class Task extends Lucid {
 
-	* index (request,response){
-
-	}
-
-	* create (request,response){
-
-	}
-	* erase (request,response){
-
-	}
-	* update (request,response){
-		
-	}
-
+	
 }
 
 module.exports = Task
